c23a0d9ddebaf47b972a86b9fe90b978de30cc75
Thomas Pointon <thomas.pointon16@imperial.ac.uk>
2018-10-15 20:50:08 +0100

    Event validation and tests

--- eventstaticdata/event_validation.go
+++ eventstaticdata/event_validation.go
@@ -1,8 +1,91 @@
 package eventstaticdata
 
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
 func validateEvent(event *Event) error {
 
-	// TODO: validate
+	if err := validateOrganiserID(event.OrganiserID); err != nil {
+		return err
+	}
+	if err := validateName(event.Name); err != nil {
+		return err
+	}
+	if err := validateLocation(event.Location); err != nil {
+		return err
+	}
+	if err := validateDates(event.StartDate, event.EndDate); err != nil {
+		return err
+	}
+	if err := validateMaxAttendance(event.MaxAttendance); err != nil {
+		return err
+	}
+
 	return nil
 
+}
+
+func validateOrganiserID(organiserID string) error {
+	if organiserID == "" {
+		return errors.New("Missing Event Organiser ID")
+	}
+	return nil
+}
+
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("Missing Event Name")
+	}
+	return nil
+}
+
+func validateLocation(location string) error {
+	if location == "" {
+		return errors.New("Missing Event Location")
+	}
+	return nil
+}
+
+func validateDates(startDate, endDate string) error {
+
+	// To be consistent with the time package
+	// (see https://stackoverflow.com/a/50557983/3837124)
+	const RFC3339FullDate = "2006-01-02"
+
+	if startDate == "" {
+		return errors.New("Missing Event Start Date")
+	}
+	if endDate == "" {
+		return errors.New("Missing Event End Date")
+	}
+	startTime, err := time.Parse(RFC3339FullDate, startDate)
+	if err != nil {
+		return errors.New("The Event Start Date must be of the form \"YYYY-MM-DD\"")
+	}
+	endTime, err := time.Parse(RFC3339FullDate, endDate)
+	if err != nil {
+		return errors.New("The Event End Date must be of the form \"YYYY-MM-DD\"")
+	}
+	if !startTime.Before(endTime) && !startTime.Equal(endTime) {
+		return errors.New("The Event Start Date must be before the End Date")
+	}
+
+	return nil
+}
+
+func validateMaxAttendance(maxAttendance string) error {
+
+	if maxAttendance == "" {
+		return errors.New("Missing Event Max Attendance")
+	}
+
+	if _, err := strconv.Atoi(maxAttendance); err != nil {
+		return errors.New("The Event Max Attendance value must be a number " +
+			" and be between 0 and 9223372036854775807")
+	}
+
+	return nil
 }
---
+++ eventstaticdata/event_validation_test.go
@@ -0,0 +1,127 @@
+package eventstaticdata
+
+import "testing"
+
+func TestInvalidEventThrowsError(t *testing.T) {
+
+	validEvent := Event{
+		Name:          "Google DevFest 2018",
+		OrganiserID:   "DocSoc",
+		Location:      "Huxley, Imperial College London",
+		StartDate:     "2018-12-05",
+		EndDate:       "2018-12-10",
+		MaxAttendance: "500"}
+	if err := validateEvent(&validEvent); err != nil {
+		t.Errorf("Not expecting an error when validating a valid event: %+v",
+			validEvent)
+	}
+
+	invalidEvent := Event{
+		Name:          "Google DevFest 2018",
+		OrganiserID:   "DocSoc",
+		Location:      "",
+		StartDate:     "2018-12-10",
+		EndDate:       "2018-12-05",
+		MaxAttendance: "many"}
+	if err := validateEvent(&invalidEvent); err == nil {
+		t.Errorf("Expecting an error when validating an invalid event: %+v",
+			invalidEvent)
+	}
+
+}
+func TestValidateOrganiserId(t *testing.T) {
+
+	if err := validateOrganiserID(""); err == nil {
+		t.Error("Expected an error when validating an empty event Organiser ID")
+	}
+
+	validOrganiserID := "DocSoc"
+	if err := validateOrganiserID(validOrganiserID); err != nil {
+		t.Errorf("Not expecting an error when validating a non-empty "+
+			"event Organiser ID: %s", validOrganiserID)
+	}
+
+}
+
+func TestValidateName(t *testing.T) {
+
+	if err := validateName(""); err == nil {
+		t.Error("Expected an error when validating an empty event Name")
+	}
+
+	validEventName := "Google DevFest 2018"
+	if err := validateOrganiserID(validEventName); err != nil {
+		t.Errorf("Not expecting an error when validating a non-empty event Name: %s",
+			validEventName)
+	}
+
+}
+
+func TestValidateLocation(t *testing.T) {
+
+	if err := validateLocation(""); err == nil {
+		t.Error("Expected an error when validating an empty event Location")
+	}
+
+	validEventLocation := "Huxley, Imperial College London"
+	if err := validateLocation(validEventLocation); err != nil {
+		t.Errorf("Not expecting an error when validating a non-empty event "+
+			"Location: %s", validEventLocation)
+	}
+
+}
+
+func TestValidateDates(t *testing.T) {
+
+	if err := validateDates("", "2018-12-05"); err == nil {
+		t.Error("Expected an error when validating an empty event Start Date")
+	}
+	if err := validateDates("2018-12-05", ""); err == nil {
+		t.Error("Expected an error when validating an empty event End Date")
+	}
+
+	invalidStartDate := "05 Dec 18 10:00 UTC"
+	if err := validateDates(invalidStartDate, "2018-12-10"); err == nil {
+		t.Errorf("Expected an error when validating an incorrectly formatted"+
+			" event Start Date: %s", invalidStartDate)
+	}
+
+	invalidEndDate := "10 Dec 18 10:00 UTC"
+	if err := validateDates("2018-12-05", invalidEndDate); err == nil {
+		t.Errorf("Expected an error when validating an incorrectly formatted"+
+			" event End Date: %s", invalidEndDate)
+	}
+
+	if err := validateDates("2018-12-10", "2018-12-05"); err == nil {
+		t.Error("Expected an error when validating start date and end dates" +
+			" that are in the wrong order")
+	}
+
+	if err := validateDates("2018-12-05", "2018-12-10"); err != nil {
+		t.Error("Not expecting an error when validating valid event dates")
+	}
+	if err := validateDates("2018-12-05", "2018-12-05"); err != nil {
+		t.Error("Not expecting an error when validating valid identical event dates")
+	}
+
+}
+
+func TestMaxAttendance(t *testing.T) {
+
+	if err := validateMaxAttendance(""); err == nil {
+		t.Error("Expecting an error when validating an empty event Max Attendance")
+	}
+
+	invalidMaxAttendance := "many"
+	if err := validateMaxAttendance("many"); err == nil {
+		t.Errorf("Expecting an error when validating an event Max Attendance"+
+			" that isn't a number: %s", invalidMaxAttendance)
+	}
+
+	validMaxAttendance := "15000"
+	if err := validateMaxAttendance(validMaxAttendance); err != nil {
+		t.Error("Not expecting an error when validating a valid event"+
+			" Max Attendance: %s", validMaxAttendance)
+	}
+
+}
